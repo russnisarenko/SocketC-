eba6cceb4e09cae7712e51501a069ca533635a30
Valery Roman <rmnvalera@gmail.com>
2018-04-11 12:43:10 +0300

    laba 1,2

---
+++ 2_NetPrServer/stdafx.h
@@ -0,0 +1,19 @@
+// stdafx.h: включаемый файл дл€ стандартных системных включаемых файлов
+// или включаемых файлов дл€ конкретного проекта, которые часто используютс€, но
+// не часто измен€ютс€
+//
+
+#pragma once
+
+#pragma comment(lib, "ws2_32.lib")
+#include "targetver.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <tchar.h>
+#include <winsock2.h>
+#include <windows.h>
+
+
+
+// TODO: ”становите здесь ссылки на дополнительные заголовки, требующиес€ дл€ программы
